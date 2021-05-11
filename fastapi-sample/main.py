337070f512f33640987ade3492bee3a99092aeef
MatyiFKBT <mmatyi1999@gmail.com>
2021-05-11 16:22:35 +0200

    [flask]

--- fastapi-sample/main.py
+++ fastapi-sample/main.py
@@ -6,4 +6,4 @@ app = App(FastAPI())
 # triggered with an HTTP request
 @app.get("/")
 def http():
-    return "Hello deta, i am running with HTTP"
+    return "Hello deta, i am running with HTTP using fastapi"
